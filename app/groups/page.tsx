629fbd805492e911fb64e0cf61ae9a5654c2ca1f
awelcing-alm <155472943+awelcing-alm@users.noreply.github.com>
2025-08-13 17:00:45 +0000

    compile

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -1,16 +1,11 @@
 // app/groups/page.tsx
-import dynamic from "next/dynamic"
+import GroupManagerPage from "@/components/pages/group-manager-page"
 import { MainLayout } from "@/components/main-layout"
 
-
-const GroupManagerPage = dynamic(() => import("@/components/pages/group-manager-page"), {
-  ssr: false,
-})
-
 export default function GroupsPage() {
-  return 
-      <MainLayout>
-<GroupManagerPage />
-      </MainLayout>
-
+  return (
+    <MainLayout>
+      <GroupManagerPage />
+    </MainLayout>
+  )
 }
